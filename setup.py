b3bea56b047829568fcdbbf7986c1327026ae07f
Pablo Moreno <pcm32@users.noreply.github.com>
2016-10-17 14:02:16 +0100

    Adds missing dependency

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
 	name	=	'uploadToMetaboLightsLabs',	# This is the name of your PyPI-package.
 	version	=	'0.1',	# Update the version number for new releases
+	install_requires = ['requests'],
 	scripts	=	['uploadToMetaboLightsLabs.py']	# The name of your scipt, and also the command you'll be using for calling it
 )
